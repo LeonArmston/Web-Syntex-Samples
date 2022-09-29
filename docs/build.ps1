a59e8fce5ea08a6c2249a6b9edd9f9cefc5d713f
Leon Armston <12968962+LeonArmston@users.noreply.github.com>
2022-09-29 17:17:55 +0100

    Update build.ps1

--- docs/build.ps1
+++ docs/build.ps1
@@ -3,8 +3,19 @@
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version 2.0
 
+$foldersToMoveToDocs = "articles","contributing","models","scenario-samples","scripts","site-templates"
+foreach($folder in $foldersToMoveToDocs)
+{
+
+  New-Item -Path ./main/docs -Name $folder -ItemType "directory"
+  copy-item -Force ./$folder/* -Destination ./docs/$folder -Recurse
+
+}
+<#
 New-Item -Path ./main/docs -Name "models" -ItemType "directory"
-copy-item -Force ./main/models/* -Destination ./main/docs/models
+copy-item -Force ./models/* -Destination ./docs/models -Recurse
+New-Item -Path ./main/scenario-samples -Name "scenario-samples" -ItemType "directory"
+copy-item -Force ./models/* -Destination ./docs/models -Recurse
 Get-item ./main/models/* | Foreach-Object {
   if($_.PSIsContainer){
       $_.BaseName
@@ -16,15 +27,19 @@ Get-item ./main/models/* | Foreach-Object {
         Copy-Item "./main/models/$($_.Name)/docs-images/*.png" -Destination "./main/docs/models/$($_.Name)/docs-images" -Force
       }
   }
-}
+}#>
+
+
 
 
 #docfx metadata ./dev/docs/docfx.json --warningsAsErrors $args
 docfx build ./main/docs/docfx.json --warningsAsErrors $args
 
 # Copy the created site to the pnpcoredocs folder (= clone of the gh-pages branch)
+Remove-Item ./gh-pages/articles/* -Recurse -Force
+Remove-Item ./gh-pages/contributing/* -Recurse -Force
+Remove-Item ./gh-pages/models/* -Recurse -Force
+Remove-Item ./gh-pages/scenario-samples/* -Recurse -Force
 Remove-Item ./gh-pages/scripts/* -Recurse -Force
-#Remove-Item ./gh-pages/sharepoint-syntex/* -Recurse -Force
-#Remove-Item ./gh-pages/contributing/* -Recurse -Force
-#Remove-Item ./gh-pages/images/* -Recurse -Force
+Remove-Item ./gh-pages/site-templates/* -Recurse -Force
 #copy-item -Force -Recurse ./main/docs/_site/* -Destination ./gh-pages
