696d0e7a2bba4f0982312a0aaaaa23748213f950
Leon Armston <12968962+LeonArmston@users.noreply.github.com>
2022-09-29 15:58:28 +0100

    Update build.ps1

--- docs/build.ps1
+++ docs/build.ps1
@@ -3,17 +3,17 @@
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version 2.0
 
-New-Item -Path ./dev/docs -Name "demos" -ItemType "directory"
-copy-item -Force ./dev/models/* -Destination ./dev/docs/demos
-Get-item ./dev/models/* | Foreach-Object {
+New-Item -Path ./main/docs -Name "demos" -ItemType "directory"
+copy-item -Force ./main/models/* -Destination ./main/docs/demos
+Get-item ./main/models/* | Foreach-Object {
   if($_.PSIsContainer){
       $_.BaseName
-      Copy-Item "./dev/models/$($_.Name)/*.md" -Destination "./dev/docs/demos/$($_.Name)" -Force
-      Copy-Item "./dev/models/$($_.Name)/*.png" -Destination "./dev/docs/demos/$($_.Name)" -Force
+      Copy-Item "./main/models/$($_.Name)/*.md" -Destination "./main/docs/demos/$($_.Name)" -Force
+      Copy-Item "./main/models/$($_.Name)/*.png" -Destination "./main/docs/demos/$($_.Name)" -Force
 
-      if(Test-Path "./dev/models/$($_.Name)/docs-images"){
-        New-Item -Path "./dev/docs/demos/$($_.Name)/" -Name "docs-images" -ItemType "directory" -Force
-        Copy-Item "./dev/models/$($_.Name)/docs-images/*.png" -Destination "./dev/docs/demos/$($_.Name)/docs-images" -Force
+      if(Test-Path "./main/models/$($_.Name)/docs-images"){
+        New-Item -Path "./main/docs/demos/$($_.Name)/" -Name "docs-images" -ItemType "directory" -Force
+        Copy-Item "./main/models/$($_.Name)/docs-images/*.png" -Destination "./dev/docs/demos/$($_.Name)/docs-images" -Force
       }
   }
 }
