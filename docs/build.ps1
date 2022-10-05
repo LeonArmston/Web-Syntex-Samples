02688f063a22cdd0d19f3a82972ffde694f1cb93
Leon Armston <12968962+LeonArmston@users.noreply.github.com>
2022-10-05 15:59:54 +0100

    Update build.ps1

--- docs/build.ps1
+++ docs/build.ps1
@@ -2,17 +2,34 @@
 
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version 2.0
-(Get-Location).Path
-$foldersToMoveToDocs = "articles","contributing","models","scenario-samples","scripts","site-templates","Leon"
+
+<#
+$foldersToMoveToDocs = "articles","contributing","models","scenario-samples","scripts","site-templates"
 foreach($folder in $foldersToMoveToDocs)
 {
 
-  New-Item -Path ./main/docs -Name $folder -ItemType "directory" -force
+  New-Item -Path ./main/docs -Name $folder -ItemType "directory"
   copy-item -Force ./main/$folder/* -Destination ./main/docs/$folder -Recurse
 
+}#>
+
+
+New-Item -Path ./main/docs -Name "models" -ItemType "directory"
+Copy-Item -Force ./main/models/* -Destination ./main/docs/models
+Get-item ./main/samples/* | Foreach-Object {
+  if($_.PSIsContainer){
+      $_.BaseName
+      Copy-Item "./main/models/$($_.Name)/*.md" -Destination "./main/docs/models/$($_.Name)" -Force
+      Copy-Item "./main/models/$($_.Name)/*.png" -Destination "./main/docs/models/$($_.Name)" -Force
+
+      if(Test-Path "./main/models/$($_.Name)/assets"){
+        New-Item -Path "./main/docs/models/$($_.Name)/" -Name "assets" -ItemType "directory" -Force
+        Copy-Item "./main/models/$($_.Name)/assets/*.png" -Destination "./main/docs/models/$($_.Name)/assets" -Force
+      }
+  }
 }
 
-docfx metadata ./main/docs/docfx.json --warningsAsErrors $args
+##docfx metadata ./main/docs/docfx.json --warningsAsErrors $args
 docfx build ./main/docs/docfx.json --warningsAsErrors $args
 
 # Copy the created site to the pnpcoredocs folder (= clone of the gh-pages branch)
